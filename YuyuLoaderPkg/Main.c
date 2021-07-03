16dedd8f8fed01d7c3c1c1e6c14ad09a69f2b978
Hiroya-W <b8122508@edu.kit.ac.jp>
2021-07-03 22:36:43 +0900

    :sparkles: WirtePixel()を使って画面を描画した
    画面に緑色の四角形を描画した

--- YuyuLoaderPkg/Main.c
+++ YuyuLoaderPkg/Main.c
@@ -8,6 +8,7 @@
 #include <Protocol/LoadedImage.h>
 #include <Protocol/SimpleFileSystem.h>
 #include <Uefi.h>
+#include "frame_buffer_config.hpp"
 
 struct MemoryMap {
     UINTN buffer_size;
@@ -321,11 +322,27 @@ EFI_STATUS EFIAPI UefiMain(EFI_HANDLE image_handle, EFI_SYSTEM_TABLE* system_tab
     // 64bit ELFにおいて、KernelMain()の実態が置かれているアドレス
     UINT64 entry_addr = *(UINT64*)(kernel_base_addr + 24);
 
+    struct FrameBufferConfig config = {(UINT8*)gop->Mode->FrameBufferBase,
+        gop->Mode->Info->PixelsPerScanLine,
+        gop->Mode->Info->HorizontalResolution,
+        gop->Mode->Info->VerticalResolution,
+        0};
+
+    switch (gop->Mode->Info->PixelFormat) {
+        case PixelRedGreenBlueReserved8BitPerColor:
+            config.pixel_format = kPixelRGBResv8BitPerColor;
+            break;
+        case PixelBlueGreenRedReserved8BitPerColor:
+            config.pixel_format = kPixelBGRResv8BitPerColor;
+            break;
+        default: Print(L"Unimplemented pixel format: %d\n", gop->Mode->Info->PixelFormat); Halt();
+    }
+
     // 引数と戻り値をvoid型とする関数型をEntryPointTypeとしてエイリアスする
-    typedef void EntryPointType(UINT64, UINT64);
+    typedef void EntryPointType(const struct FrameBufferConfig*);
     EntryPointType* entry_point = (EntryPointType*)entry_addr;
     // カーネルを起動する
-    entry_point(gop->Mode->FrameBufferBase, gop->Mode->FrameBufferSize);
+    entry_point(&config);
 
     Print(L"All done\n");
 
